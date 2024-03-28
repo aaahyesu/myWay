c354ab42c4ff7014aa3e158ae53c078763715b8b
lee <rrtu123@naver.com>
2024-03-28 17:50:04 +0900

    commuDetail-ui

--- app/(tap)/community/page.tsx
+++ app/(tap)/community/page.tsx
@@ -1,12 +1,51 @@
-import type { NextPage } from "next";
-import React from 'react';
+"use client";
+import { NextPage } from "next";
+import React, { useState } from 'react';
+import Link from "next/link";
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+
 
 
 const Community: NextPage = () => {
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+    setCurrentIndex(slideIndex);
+  };
     return(
         <div className="px-1">
             {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-            <div key={i} className="mt-4">
+            <div key={i} className="mt-8">
                 <div className="flex space-x-3 mb-2">
                     <img
                         className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
@@ -20,149 +59,38 @@ const Community: NextPage = () => {
                     <div className="flex flex-grow justify-end"></div>
                     <button className="inline-flex items-center font-Nanum rounded-2xl bg-gray-50 px-4 text-xs text-gray-600 ring-1 ring-inset ring-gray-500/10">follow</button>
                 </div>
-                <div
-                    id="carouselExampleIndicators"
-                    className="relative"
-                    data-twe-carousel-init
-                    data-twe-ride="carousel"
-                  >
-      {/* Carousel indicators */}
-      <div
-        className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
-        data-twe-carousel-indicators
-      >
-        <button
-          type="button"
-          data-twe-target="#carouselExampleIndicators"
-          data-twe-slide-to="0"
-          data-twe-carousel-active
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-twe-target="#carouselExampleIndicators"
-          data-twe-slide-to="1"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-twe-target="#carouselExampleIndicators"
-          data-twe-slide-to="2"
-          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-
-      {/* Carousel items */}
-      <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-        {/* First item */}
-        <div
-          className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-item
-          data-twe-carousel-active
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-            className="block w-full"
-            alt="Wild Landscape"
-          />
-        </div>
-        {/* Second item */}
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-item
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-            className="block w-full"
-            alt="Camera"
-          />
-        </div>
-        {/* Third item */}
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-twe-carousel-item
-        >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-            className="block w-full"
-            alt="Exotic Fruits"
-          />
-        </div>
-      </div>
-
-      {/* Carousel controls - prev item */}
-      <button
-        className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-twe-target="#carouselExampleIndicators"
-        data-twe-slide="prev"
-      >
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Previous
-        </span>
-      </button>
-
-      {/* Carousel controls - next item */}
-      <button
-        className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-twe-target="#carouselExampleIndicators"
-        data-twe-slide="next"
-      >
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Next
-        </span>
-      </button>
-    </div>
-                {/* <div id="indicators-carousel" className="relative w-full" data-carousel="static">
-                    <img
-                            className="w-full"
-                            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                    />
-                </div> */}
-                <div className="mt-2 flex space-x-2">
-                    <h1 className="font-Nanum font-semibold">Title - instagram copycat</h1>
+                <div className='w-[380px] h-[380px] m-auto relative group'>
+                  <div
+                    style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+                    className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+                  ></div>
+                  {/* Left Arrow */}
+                  <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-0.5 text-white cursor-pointer'>
+                    <BsChevronCompactLeft onClick={prevSlide} size={30} />
+                  </div>
+                  {/* Right Arrow */}
+                  <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-0.5 text-white cursor-pointer'>
+                    <BsChevronCompactRight onClick={nextSlide} size={30} />
+                  </div>
+                  <div className='flex top-4 justify-center py-2'>
+                    {slides.map((slide, slideIndex) => (
+                      <div
+                        key={slideIndex}
+                        onClick={() => goToSlide(slideIndex)}
+                        className='text-2xl cursor-pointer'
+                      >
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <Link href="/comdetail">
+                <div className="mt-2 flex space-x-2 px-2">
+                    <h1 className="font-Nanum text-black hover:text-gray-500 font-semibold">Title - instagram copycat</h1>
                     <span className="inline-flex font-Nanum items-center rounded-lg bg-blue-400 px-2  text-sm text-white">맛집</span>
                     <span className="inline-flex font-Nanum  items-center rounded-lg  bg-blue-400 px-2 text-sm text-white">관광지</span>
                 </div>
-                <h1 className="mt-1 font-Nanum text-sm">detail ~~~~~~</h1>
+                <h1 className="mt-1 px-2 text-black hover:text-gray-500 font-Nanum text-sm">detail ~~~~~~</h1>
+                </Link>
             </div>
             ))}
         </div>
